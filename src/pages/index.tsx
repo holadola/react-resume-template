8c7b3e818b0c3c81ce7de64100f15242aaaae0a6
carl.christiansen <carl.christiansen@sap.com>
2025-01-21 22:04:03 +0100

    Fix imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,11 @@ import {FC, memo} from 'react';
 
 import Page from '../components/Layout/Page';
 import About from '../components/Sections/About';
+import Footer from '../components/Sections/Footer';
 import Hero from '../components/Sections/Hero';
 import Resume from '../components/Sections/Resume';
 import Testimonials from '../components/Sections/Testimonials';
 import {homePageMeta} from '../data/data';
-import Footer from '../components/Sections/Footer';
 
 // eslint-disable-next-line react-memo/require-memo
 const Header = dynamic(() => import('../components/Sections/Header'), {ssr: false});
