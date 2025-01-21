dcd368c9676f3e3faa095017f2eb454e89cd9221
carl.christiansen <carl.christiansen@sap.com>
2025-01-21 21:46:32 +0100

    Fix linting

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -1,18 +1,12 @@
 import {
   AcademicCapIcon,
   ArrowDownTrayIcon,
-  BuildingOffice2Icon,
   CalendarIcon,
   FlagIcon,
   MapIcon,
-  SparklesIcon,
 } from '@heroicons/react/24/outline';
 
-import GithubIcon from '../components/Icon/GithubIcon';
-import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
-import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
-import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
 import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
 import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
@@ -302,13 +296,13 @@ export const education: TimelineItem[] = [
     date: 'January 2023',
     location: 'AWS',
     title: 'AWS Certified Solution Architect – Associate',
-    //content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    content: <p></p>,
   },
   {
     date: 'August 2000 - August 2005',
     location: 'Uppsala University',
     title: 'Information Technology Engineering',
-    //content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    content: <p></p>,
   },
 ];
 
